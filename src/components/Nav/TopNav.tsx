0805f1c22e0e77066b88cc2ba4904ea14a95d903
jung518 <wjddndus0518@naver.com>
2026-06-17 14:10:56 +0900

    Fix: 수정 디자인 반영

--- src/components/Nav/TopNav.tsx
+++ src/components/Nav/TopNav.tsx
@@ -13,7 +13,7 @@ export default function TopNav({ name }: Props) {
       <View style={styles.left}>
         {/* 백엔드 연결 시 Image 컴포넌트로 교체 */}
         <View style={styles.profile} />
-        <Typography size="xxl" weight="bold" style={styles.title}>
+        <Typography size="xxl" weight="semiBold" style={styles.title}>
           {name} 님
         </Typography>
       </View>
@@ -47,6 +47,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
+    backgroundColor: '#FFF',
   },
   left: {
     flexDirection: 'row',
@@ -64,11 +65,11 @@ const styles = StyleSheet.create({
   title: {
     color: '#222',
     textAlign: 'center',
-    letterSpacing: 1,
+    letterSpacing: 0.6,
   },
   icons: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 5,
+    gap: 8,
   },
 });
